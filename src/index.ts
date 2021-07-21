fadde7c0b06f6e40ec73ea9c96f85956d93b4a51
Enterprise09 <goldenstaytes@gmail.com>
2021-07-21 16:41:28 +0900

    Use Class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+//Interface
+/*
 interface Human {
   name: string;
   age: number;
@@ -9,10 +11,25 @@ const person = {
   gender: "male",
   age: 22,
 };
+*/
+
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  } //end of constructor
+}
+
+const enter = new Human("Enter", 21, "male");
+
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(enter));
 
 export {};
